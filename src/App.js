0a24ec8ac54559b4d58ee7fd3d774b9a53a7f789
rumeysakyrk <rumeysakyrk@gmail.com>
2023-02-20 21:36:55 +0300

    convert js from html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
+  const para =document.createElement('p');
+  para.textContent= 'This is also visible!!';
+  document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
